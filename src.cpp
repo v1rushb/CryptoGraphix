50392f28b24a2115c967e174c02bbfec0411dba5
v1rushb <211111@ppu.edu.ps>
2024-02-28 16:31:36 +0200

    NPCR, UACI & HD Added

--- src.cpp
+++ src.cpp
@@ -10,6 +10,8 @@
 using namespace std;
 using namespace CryptoPP;
 
+#define ll long long int
+
 string outputDirectory = "./resources/";
 
 vector<char> readImage(const string &imagePath) {
@@ -78,6 +80,50 @@ vector<char> decryptImage(const vector<char> &data, const SecByteBlock &key, con
     return vector<char>(decrypted.begin(), decrypted.end());
 }
 
+double NPCR(const vector<char> &img1, const vector<char> &img2) {
+    if(img1.size() - img2.size())
+    {
+        cerr << "SizeDifferenceException" << endl;
+        exit(1);
+    }
+    
+    ll diff(0);
+    for(ll o = 0; o < img1.size();o++)
+        diff+=img1[o]!=img2[o];
+    return (diff / (double) img1.size()) * 100.0;
+}
+
+double UACI(const vector<char> &img1, const vector<char> &img2) {
+    if(img1.size()-img2.size())
+    {
+        cerr << "SizeDifferenceException" << endl;
+        exit(1);
+    }
+    double s(0);
+    for(ll o = 0; o < img1.size();o++) {
+        s+=abs(img1[o]-'0' - img2[o]-'0')/255.0;
+    }
+    return (s/img1.size())*100.0;
+}
+
+double hammingDistance(const vector<char> &img1, const vector<char> &img2) {
+    if(img1.size()-img2.size()) 
+    {
+        cerr << "SizeDiffernceException" << endl;
+        exit(1);
+    }
+
+    ll dist(0);
+    for(int o = 0; o < img1.size();o++)
+    {
+        ll x = (img1[o] - '0') ^ (img2[o]-'0');
+        for(int bit = 0; bit < 8;bit++) {
+            dist += (x >> bit) &1;
+        }
+    }
+    return (dist/ (double) (img1.size()*8)) * 100;
+}
+
 int main() {
     string imagePath = "./Sasha.jpg";
     string encryptedPath = outputDirectory + "encrypted_img.jpeg";
@@ -88,6 +134,8 @@ int main() {
     }
 
     vector<char> imageData = readImage(imagePath);
+    vector<char> modifiedImageData = imageData;
+    modifiedImageData[0] ^= 1;
     if (imageData.empty()) {
         cout << "Failed to read image." << endl;
         return 1;
@@ -100,6 +148,7 @@ int main() {
     prng.GenerateBlock(iv, sizeof(iv));
 
     vector<char> encryptedImg = encryptImage(imageData, key, iv);
+    vector<char> encryptedModifiedImg = encryptImage(modifiedImageData,key,iv);
     if (writeImage(encryptedPath, encryptedImg)) {
         cout << "Encrypted image written to: " << encryptedPath << endl;
     } else {
@@ -112,6 +161,10 @@ int main() {
     } else {
         return 1;
     }
+
+    cout << "NPCR: " << NPCR(encryptedImg,encryptedModifiedImg) << '%' << endl;
+    cout << "UACI: " << UACI(encryptedImg,encryptedModifiedImg) << '%' << endl;
+    cout << "HD: " << hammingDistance(encryptedImg,encryptedModifiedImg) << '%' << endl;
 
     return 0;
 }
